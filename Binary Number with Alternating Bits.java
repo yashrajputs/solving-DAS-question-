94f63ba004a741b30f5f8658631eca8d6b3a7c45
Yash Rajput <122536776+yashrajputs@users.noreply.github.com>
2026-02-18 10:11:34 +0530

    Add solution for checking alternating bits in binary number
    Implemented three approaches to check if a binary number has alternating bits using string manipulation, bitwise operations, and XOR.

---
+++ Binary Number with Alternating Bits.java
@@ -0,0 +1,52 @@
+/******************************************************* JAVA *******************************************************/
+//Approach-1 (Using shift operators and Xor operation)
+//T.C : O(log(n))
+//S.C : O(1)
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        String b = Integer.toBinaryString(n);
+
+
+        for(int i = 0; i < b.length() -1; i++){
+            if(b.charAt(i) == b.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+//Approach-2 (Using shift operators and Xor operation)
+//T.C : O(log(n))
+//S.C : O(1)
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int currBit = n % 2;
+        n /= 2;
+
+        while (n > 0) {
+            if (currBit == n % 2) {
+                return false;
+            }
+
+            currBit = n % 2;
+            n /= 2;
+        }
+
+        return true;
+    }
+}
+
+
+
+//Approach-3 (Using shift operators and Xor operation)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int result = n ^ (n >> 1); 
+        return (result & (result + 1)) == 0;
+    }
+}
