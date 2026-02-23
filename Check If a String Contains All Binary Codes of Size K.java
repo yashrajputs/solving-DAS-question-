ad9cbd525421b54d781917cb655e2d4b3f5c6fce
Yash Rajput <122536776+yashrajputs@users.noreply.github.com>
2026-02-23 09:54:18 +0530

    Refactor hasAllCodes method and comments
    Fix comment formatting and update logic for unique substrings.

---
+++ Check If a String Contains All Binary Codes of Size K.java
@@ -0,0 +1,27 @@
+/******************************************************* JAVA *******************************************************/
+//Approach (Brute Force All Possible Answers)
+//T.C : O(n*k)
+//S.C : O(2^k * k), storing 2^k substrings of length k each
+
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        if(s.length() < k)
+        return false;
+
+        int uniqueSub = 1<<k;
+        HashSet<String> st = new HashSet<>();
+
+        for(int i = k; i <= s.length(); i++) {
+            String sub = s.substring(i-k, i);
+
+            if(!st.contains(sub)) {
+                st.add(sub);
+                uniqueSub--;
+            }
+            if(uniqueSub == 00 )
+            return true;
+        }
+        return false;
+        
+    }
+}
