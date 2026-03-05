e0f29beb518ce610d01dc2f59103b23f80f169fe
Yash Rajput <122536776+yashrajputs@users.noreply.github.com>
2026-03-05 08:46:15 +0530

    Create Minimum Changes To Make Alternating Binary String.java

---
+++ Minimum Changes To Make Alternating Binary String.java
@@ -0,0 +1,61 @@
+/************************************************************ JAVA **********************************************************************/
+//Approach-1 (String can start from 0, string can start from 1)
+//T.C : O(n)
+//S.C : O(n)
+public class Solution {
+    public int minOperations(String s) {
+        int n = s.length();
+        int start_with_0 = 0;
+        int start_with_1 = 0;// 010101
+        // 101010
+
+        
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                if (s.charAt(i) == '0') {
+                    start_with_1++;
+                } else {
+                    start_with_0++;
+                }
+            } else {
+                if (s.charAt(i) == '1') {
+                    start_with_1++;
+                } else {
+                    start_with_0++;
+                }
+            }
+        }
+
+        return Math.min(start_with_0, start_with_1);
+    }
+}
+
+
+//************************************************************ JAVA **************************************************      
+//Approach-2 (No need to calculate both)
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    public int minOperations(String s) {
+        int n = s.length();
+        int start_with_0 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                if (s.charAt(i) == '1') {
+                    start_with_0++;
+                }
+            } else {
+                if (s.charAt(i) == '0') {
+                    start_with_0++;
+                }
+            }
+        }
+
+        return Math.min(start_with_0, n - start_with_0);
+    }
+}
+
+       
+    }
+}
