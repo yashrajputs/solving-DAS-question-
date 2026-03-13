d60dcdde6fe8225a898730609e9e05883089db29
Yash Rajput <122536776+yashrajputs@users.noreply.github.com>
2026-03-13 22:01:04 +0530

    Create Minimum Number of Seconds to Make Mountain Height Zero.java

---
+++ Minimum Number of Seconds to Make Mountain Height Zero.java
@@ -0,0 +1,36 @@
+//Approach (Binary search on answer)
+//T.C : O(n * log(Tmax * mountainHeight^2), where Tmax = maximum time among all workers in the input, n = length of workerTimes
+//S.C : O(1)
+
+class Solution {
+    private boolean check(long mid, int[] workerTimes, int mH){
+        long h = 0;
+        for(int t : workerTimes) {
+            h += (long)(Math.sqrt(2.0*mid / t + 0.25)-0.5);
+            if(h >= mH){
+                return true;
+            }
+        }
+        return h >= mH;
+    }
+    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+        int maxTime = 0;
+        for(int t : workerTimes)
+        maxTime = Math.max(maxTime, t);
+        long l = 1;
+        long r = (long) maxTime * mountainHeight * (mountainHeight + 1) / 2;
+        long result = 0;
+
+        while(l <= r) {
+            long mid =  l + (r - l) / 2;
+            if(check(mid, workerTimes,mountainHeight)) {
+                result = mid;
+                r = mid - 1;
+            }else {
+                l = mid + 1;
+            }
+        }
+        return result;
+        
+    }
+}
