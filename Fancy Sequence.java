6b7b77b9b78d30e900ecf589ccdf9e13b096a2e0
Yash Rajput <122536776+yashrajputs@users.noreply.github.com>
2026-03-15 09:54:33 +0530

    Create Fancy Sequence.java

---
+++ Fancy Sequence.java
@@ -0,0 +1,54 @@
+//T.C : O(log(M)) for Binary Exponentiation power(mult, M-2)
+//S.C : O(1)
+class Fancy {
+    long M = 1_000_000_007;
+
+    List<Long> seq = new ArrayList<>();
+    long add = 0;
+    long mult = 1;
+
+  //Binary Exponentiation for Fermat's Little Theorem -> power(mult, M-2);
+
+    long power(long a, long b) {
+        if (b == 0)
+            return 1;
+
+        long half   = power(a, b / 2);
+        long result = (half * half) % M;
+
+        if (b % 2 == 1) {
+            result = (result * a) % M;
+        }
+
+        return result;
+    }
+
+
+    public Fancy() {
+        
+    }
+    
+    public void append(int val) {
+        long x = ((val - add) % M + M) * power(mult, M - 2) % M;
+        seq.add(x);
+        
+    }
+    
+    public void addAll(int inc) {
+        add = (add + inc) % M;
+    }
+    
+    public void multAll(int m) {
+        mult = (mult * m) % M;
+        add = (add * m) % M;
+    }
+    
+    public int getIndex(int idx) {
+        if(idx >= seq.size())
+        return -1;
+
+        return (int) ((seq.get(idx) * mult + add) % M);
+        
+    }
+}
+
