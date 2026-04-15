176faeffc6dac73a9ba570a1403c8730ce6facca
Yash Rajput <122536776+yashrajputs@users.noreply.github.com>
2026-04-15 07:04:19 +0530

    Add closestTarget method for circular distance calculation
    Implement method to find the shortest distance to a target string in a circular array.

---
+++ Shortest Distance to Target String in a Circular Array .java
@@ -0,0 +1,22 @@
+//Approach (Simple simulation)
+//T.C : O(n*L), L = average length of all the words
+//S.C : O(1)
+
+class Solution {
+    public int closestTarget(String[] words, String target, int startIndex) {
+
+        int n  =  words.length;
+        int result = Integer.MAX_VALUE;
+
+        for(int i = 0 ; i < n ; i++){
+            if(words[i].equals(target)) {
+                int straightDist =  Math.abs(i - startIndex);
+                int circularDist = n - straightDist;
+
+                result =  Math.min(result, Math.min(straightDist, circularDist));
+            }
+        }
+        return result == Integer.MAX_VALUE ? -1 : result;
+        
+    }
+}
