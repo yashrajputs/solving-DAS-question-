c8745d1b7c696146e7c43a739a0d8a773cbcf463
Yash Rajput <122536776+yashrajputs@users.noreply.github.com>
2026-04-22 09:48:21 +0530

    Add twoEditWords method for query comparison
    Implement method to find words within two edits of a dictionary.

---
+++ Words Within Two Edits of Dictionary.java
@@ -0,0 +1,29 @@
+//Approach (Iterate and check)
+//T.C : O(q * d * n), q = queries.size(), d = dictionary.size(), n = query[i].length()
+//S.C : O(1)
+
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> result = new ArrayList();
+
+        for(String query : queries) {
+            
+            for(String s : dictionary) {
+                int diff = 0;
+
+                for(int i = 0; i < query.length(); i++) {
+                    if(query.charAt(i) != s.charAt(i)) {
+                        diff++;
+                    }
+                    if(diff > 2)
+                    break;
+                }
+                if(diff <= 2){
+                    result.add(query);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
