1287e4875ce9b1f1452e513566f78dacbafef36d
Yash Rajput <122536776+yashrajputs@users.noreply.github.com>
2026-05-01 20:15:25 +0530

    Add maxRotateFunction to compute max rotation sum
    Implement maxRotateFunction to calculate maximum rotation sum.

---
+++ Rotate Function.java
@@ -0,0 +1,27 @@
+//Approach (Compute relation and find F(K+1) from F(K) and so on
+//T.C : O(n)
+//S.C : O(1)
+
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+         int n =  nums.length;
+
+         int sum  = 0;
+          int F = 0;
+
+          for(int i = 0; i < n; i++) {
+            sum += nums[i];
+            F += i * nums[i];
+          }
+          int result = F;
+
+          for(int k = 0; k < n; k++) {
+            int newF= F + sum - n * nums[n - 1 - k];
+            result = Math.max(result, newF);
+            F = newF;
+          }
+
+          return result;
+        
+    }
+}
